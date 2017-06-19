2e08693075432d0c384dc8c7220b8e846500f61c
Matthew Davidson <matthew@modulolotus.net>
2017-06-19 14:26:09 -0400

    Updated Lotus theme. Brought up-to-date with latest Cryogen.

--- cryogen/project.clj
+++ cryogen/project.clj
@@ -4,12 +4,12 @@
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.8.0"]
-                           [ring/ring-devel "1.5.1"]
-                           [compojure "1.5.2"]
+                           [ring/ring-devel "1.6.1"]
+                           [compojure "1.6.0"]
                            [ring-server "0.4.0"]
                            [cryogen-markdown "0.1.6"]
-                           [cryogen-core "0.1.54"]
-                           ;; [org.clojure/tools.nrepl "0.2.12"]
+                           [cryogen-core "0.1.56"]
+                           [org.clojure/tools.nrepl "0.2.13"]
                            ]
             :plugins [[lein-ring "0.9.7"]]
             :main cryogen.core
