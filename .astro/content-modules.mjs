8cb3594ba42bd2f2b86f3c24b1e6833e9330ec4c
Matthew Davidson <matthew@modulolotus.net>
2025-03-19 19:02:22 +0700

    Fixed ignored files added by accident when moving .gitignore around Untracked node_modules/ Untracked .astro/

--- .astro/content-modules.mjs
+++
@@ -1,4 +0,0 @@
-
-export default new Map([
-["src/content/blog/2018-05-31-clojure-trie-performance.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F2018-05-31-clojure-trie-performance.mdx&astroContentModuleFlag=true")]]);
-		
